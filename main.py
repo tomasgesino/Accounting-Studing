8dab7141c03e60d78669fc3a2fcb3d9ebff62b3d
Tomas Gesino <35117581+tomasgesino@users.noreply.github.com>
2018-08-22 23:38:26 -0300

    main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ car = 12000
 carmarketp = 10000
 rescar = car * 0.10
 
-userdepreciationdecision = input()
+userdepreciationdecision = input("What type of depresciation do you want? ")
 
 def straightln():
     b = car - rescar
@@ -20,7 +20,6 @@ def revl():
     print(c)
     print("On good T Credit & on depresciation Debit")
 
-
 if userdepreciationdecision == 1:
     straightln()
 elif userdepreciationdecision == 2:
@@ -29,3 +28,36 @@ elif userdepreciationdecision == 3:
     revl()
 else:
     print("Error 404 ; Command not found")
+
+
+userpruchase = input("What where your purchases? ")
+userpruchasesreturns = input("What where your purchases returns? ")
+useropeningstock = input("What was your opening stock? ")
+userclosingstock = input("What was your closing stock? ")
+usercarrigeinwards = input("What where your carrige inwards? ")
+
+usersales = input("What where your sales? ")
+usersalesreturns = input("What where your sales returns? ")
+
+userinsurance = input("What is the cost of insurance? ")
+userrent = input("What is the cost of rent? ")
+userwages = input("What is the cost of wages? ")
+
+def incomestate():
+    costofsales = userpruchase - userpruchasesreturns + useropeningstock - userclosingstock + usercarrigeinwards
+    sales = usersales - usersalesreturns
+    grossprofit = costofsales - sales
+    expenses = userinsurance + userrent + userwages
+    netprofit = grossprofit - expenses
+    print("Cost of Sales: ")
+    print(costofsales)
+    print("Sales: ")
+    print(sales)
+    print("Gross Profit: ")
+    print(grossprofit)
+    print("Expenses: ")
+    print(expenses)
+    print("Net Profit: ")
+    print(netprofit)
+
+incomestate()
